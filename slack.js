a72f01ccdd614a8a8d2703199344b269331d0733
Your Name <you@example.com>
2018-02-26 15:42:33 +0530

    first commit

--- slack.js
+++ slack.js
@@ -1,71 +1,68 @@
-var config = require('./config');
 var responses = {};
-
-responses.quickReplies  = function(sessionId, content, params,context){
-	console.log(content,incidentParams[sessionId]['recentInput']);	  
-	return {			
-		"speech": "",
-		"contextOut": [{
-			 "name":context, 
-			 "lifespan":2, 
-			 "parameters":params
-		}],
-		"messages": [{
-			  "type": 2,
-			  "platform": "slack",
-			  "title": "Please select option from "+incidentParams[sessionId]['recentInput'],
-			  "replies": content
-		},
-		{
-		  "type": 0,
-		  "speech": ""
+responses.getResponse = function(demotype){
+	return new Promise(function(resolve, reject){
+		switch(demotype.toLowerCase()){
+			case "text response":case "simple response":resolve(simpleResponse());break;
+			case "card":case "basic card":resolve(basicCard());break;
+			case "quick replies":case "suggestionChips":resolve(quickReplies());break;
+			case "image":resolve(image());break;
 		}
-		]
-	};
+	});
+}
+quickReplies  = function(){	
+	return new Promise(function(resolve, reject){
+		resolve( {			
+			"speech": "",		
+			"messages": [{
+				  "type": 2,
+				  "platform": "slack",
+				  "title": "Please select option from quick replies",
+				  "replies": ["Demo1","Demo2"]
+			},
+			{
+			  "type": 0,
+			  "speech": ""
+			}
+			]
+		});	
+	});
 	  //console.log('hari');
 	//return true;
 }
-
-responses.simpleText = function (sessionId, promptMsg, params, context){
-	 return {			
-		"speech": "",		
-		"contextOut": [{
-			 "name":context, 
-			 "lifespan":2, 
-			 "parameters":params
-		}],				
-		"messages": [{
-		  "type": 0,
-		  "platform": "slack",
-		  "speech": promptMsg
-		},	
-		{
-		  "type": 0,
-		  "speech": ""
-		}]
-	};	
-}
-
-responses.getFinalCardResponse = function(textMsg, callBackIntent, params){
+simpleResponse = function (){
 	return new Promise(function(resolve, reject){
-		var data = textMsg.split(';');		
-		
-		var rsp ={
+		resolve({			
+			"speech": "",								
+			"messages": [{
+			  "type": 0,
+			  "platform": "slack",
+			  "speech": "Simple text Reponse demo"
+			},	
+			{
+			  "type": 0,
+			  "speech": ""
+			}]
+		});	
+	});
+}
+basicCard = function(){
+	return new Promise(function(resolve, reject){				
+		resolve({
 			"speech": "",
 			"messages": [{
 				  "type": 1,
 				  "platform": "slack",
-				  "title": data[2]+"\n"+data[0],
-				  "subtitle": data[1]+"\nThank you for using me, I can help you please choose any one option",
+				  "title": "basic card",
+				  "subtitle": "demo",
 				  "imageUrl": "https://raw.githubusercontent.com/39781/incidentMG/master/images/incidentMG.jpg",
 				  "buttons": [
 					{
-					  "text": "Create Incident",
-					  "postback": "Create Incident"
+					  "text": "Demo1",
+					  "postback": "Demo1"
 					},
 					{
-					  "text": "Track Incident",
-					  "postback": ""
+					  "text": "Demo2",
+					  "postback": "Demo2"
 					}
 				  ]
 				},				 
@@ -73,46 +70,24 @@ responses.getFinalCardResponse = function(textMsg, callBackIntent, params){
 					"type": 0,
 					"speech": ""
 				}]
-		};
-		if(callBackIntent){
-			rsp.followupEvent ={
-				name:callBackIntent,
-				data:params,
-			}
-		}			
-		resolve(rsp);
+		});
 	});
 }
 		
-responses.getFinalSimpleResponse = function(txtMsg, callBackIntent, params){
+image = function(txtMsg, callBackIntent, params){
 	return new Promise(function(resolve, reject){	
-		var data = txtMsg.split(';');			
-		
-	var rsp ={			
+		resolve({			
 				"speech": "",					
 				"messages": [{
-				  "type": 0,
+				  "type": 3,
 				  "platform": "slack",
-				  "speech": data[2]+"\n"+data[0]+"\n"+data[1]+"\n Thank you for using me\nI can help you"
-				},
-				{
-				  "type": 2,
-				  "platform": "slack",
-				  "title": "Please select option",
-				  "replies": ["Create Incident","Track Incident"]
+				  "imageUrl": "https://raw.githubusercontent.com/39781/incidentMG/master/images/incidentMG.jpg"
 				},		
 				{
 				  "type": 0,
 				  "speech": ""
 				}]
-			};
-		if(callBackIntent){
-			rsp.followupEvent ={
-				name:callBackIntent,
-				data:params,
-			}
-		}			
-		resolve(rsp);
+			});		
 	});
 }
 
