3044465b176a60bb4360e1684e7dc986e47d799a
bhariprasad-msc <bhariprasad.msc@gmail.com>
2018-03-05 21:15:42 +0530

    first commit

--- routes.js
+++ routes.js
@@ -11,7 +11,7 @@ module.exports = function(router, passport){
 	});
 	router.get('/sendResponseToBot',isLoggedIn, function(req, res){
 		console.log('sendresponsebot',req.user);
-		sendMessageToBot(req.user,req,session.senderId);
+		sendMessageToBot(req.user,req.session.senderId);
 		res.sendFile(path.resolve('./public/closeWindow.html'));
 	})
 	router.get('/auth/facebook', passport.authenticate('facebook', { 
