5003f1e6f29680b0537459c8ec7fa8f40002573c
bhariprasad-msc <bhariprasad.msc@gmail.com>
2018-03-05 20:47:51 +0530

    first commit

--- auth/passport.js
+++ auth/passport.js
@@ -15,17 +15,12 @@ module.exports = function(passport) {
 
     // used to serialize the user for the session
     passport.serializeUser(function(user, done) {
-        done(null, user.id);
+        done(null, user);
     });
 
     // used to deserialize the user
-    passport.deserializeUser(function(id, done) {
-		if(User.facebook[id]){
-			done(null,User.facebook[id]);	
-		}
-		if(User.google[id]){
-			done(null,User.facebook[id]);	
-		}		
+    passport.deserializeUser(function(user, done) {
+		done(null, user);
         /*User.findById(id, function(err, user) {
             done(err, user);
         });*/
