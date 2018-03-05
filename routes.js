b6ab5af9f12debc01a0ecafb151d346247c3fc0a
39781.hexaware@gmail.com <bhariprasad.msc@gmail.com>
2018-03-05 23:16:07 +0530

    first commit

--- routes.js
+++ routes.js
@@ -12,7 +12,7 @@ module.exports = function(router, passport){
 	});
 	router.get('/sendResponseToBot',isLoggedIn, function(req, res){		
 		var queryParam = url.parse(req.headers.referer, true);
-		console.log(req.query,req.user,'user info');
+		console.log(req.headers.referer,req.query,req.user,'user info');
 		sendMessageToBot(req.query['src'],req.user,queryParam.query['rid']);
 		res.sendFile(path.resolve('./public/closeWindow.html'));
 	})
